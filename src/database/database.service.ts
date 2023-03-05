03221f4eb23852dbc7e4d23f524099a1a296d19b
moccot <justother94@gmail.com>
2023-03-05 14:34:41 -0400

    Add read routes

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,4 +1,4 @@
-import {ConflictException, Injectable, InternalServerErrorException} from "@nestjs/common";
+import {ConflictException, Injectable, InternalServerErrorException, NotFoundException} from "@nestjs/common";
 import {DataSource} from "typeorm";
 import {TodoEntity} from "./entities/todo.entity";
 import {TodoDto} from "../todo/dtos/todo.dto";
@@ -34,5 +34,41 @@ export class DatabaseService {
         }
 
         return newTodo;
+    }
+
+    async getAllTodos(): Promise<TodoEntity[]> {
+        return await this.dataSource.manager.find(TodoEntity);
+    }
+
+    private async getTodosBy(whereOption: any): Promise<TodoEntity[]> {
+        return await this.dataSource.manager.findBy(TodoEntity, whereOption);
+    }
+
+    private async getTodoBy(whereOption: any): Promise<TodoEntity> {
+        let todo: TodoEntity | null = await this.dataSource.manager.findOneBy(TodoEntity, whereOption);
+
+        if (todo === null) {
+            throw new NotFoundException({
+                message: "This todo does not exist."
+            });
+        }
+
+        return todo;
+    }
+
+    async getTodoById(todoId: string): Promise<TodoEntity> {
+        return await this.getTodoBy({id: todoId});
+    }
+
+    async getTodoByDescription(todoDescription: string): Promise<TodoEntity> {
+        return await this.getTodoBy({description: todoDescription});
+    }
+
+    async getAllCheckedTodos(): Promise<TodoEntity[]> {
+        return await this.getTodosBy({checked: true});
+    }
+
+    async getAllUncheckedTodos(): Promise<TodoEntity[]> {
+        return await this.getTodosBy({checked: false});
     }
 }
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, Body, UsePipes, Post} from "@nestjs/common";
+import {Controller, Get, Body, UsePipes, Post, Param} from "@nestjs/common";
 import {CustomResponseBodyInterface} from "../interfaces/custom-response-body.interface";
 import {DatabaseService} from "../database/database.service";
 import {TodoDataSchema, TodoDto} from "./dtos/todo.dto";
@@ -8,6 +8,7 @@ import {TodoEntity} from "../database/entities/todo.entity";
 @Controller("todo")
 export class TodoController {
     constructor(private readonly databaseService: DatabaseService) {}
+
     @Post("add")
     @UsePipes(new JoiValidationPipe(TodoDataSchema))
     async create(@Body() todoDto: TodoDto): Promise<CustomResponseBodyInterface> {
@@ -21,8 +22,51 @@ export class TodoController {
 
     @Get()
     async getAll(): Promise<CustomResponseBodyInterface> {
+        let todos: TodoEntity[] = await this.databaseService.getAllTodos();
+
         return {
-            message: "OK"
+            message: "Listing all todos.",
+            data: todos
+        };
+    }
+
+    @Get("id/:id")
+    async getById(@Param("id") todoId: string): Promise<CustomResponseBodyInterface> {
+        let todo: TodoEntity = await this.databaseService.getTodoById(todoId);
+
+        return {
+            message: `Listing todo of id: ${todoId}.`,
+            data: todo
+        };
+    }
+
+    @Get("description/:description")
+    async  getByDescription(@Param("description") todoDescription: string): Promise<CustomResponseBodyInterface> {
+        let todo: TodoEntity = await  this.databaseService.getTodoByDescription(todoDescription);
+
+        return {
+            message: `Listing todo of description: ${todoDescription}.`,
+            data: todo
+        };
+    }
+
+    @Get("checked")
+    async getAllChecked(): Promise<CustomResponseBodyInterface> {
+        let todos: TodoEntity[] = await this.databaseService.getAllCheckedTodos();
+
+        return {
+            message: "Listing all checked todos.",
+            data: todos
+        };
+    }
+
+    @Get("unchecked")
+    async getAllUnchecked(): Promise<CustomResponseBodyInterface> {
+        let todos: TodoEntity[] = await this.databaseService.getAllUncheckedTodos();
+
+        return {
+            message: "Listing all unchecked todos.",
+            data: todos
         };
     }
 }
